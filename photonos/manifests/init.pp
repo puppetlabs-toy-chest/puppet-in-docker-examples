844d0ba9441cab4f10365c3309705f5bc5f43e6a
Gareth Rushgrove <gareth@morethanseven.net>
2016-10-19 09:56:55 -0700

    Swap the nginx example for an etcd example
    The nginx package has been taken out of the Photon repositories.

--- photonos/manifests/init.pp
+++ photonos/manifests/init.pp
@@ -1,10 +1,29 @@
-package { 'nginx':
-	ensure => latest,
+$etcd_service = @("SERVICE"/L)
+[Unit]
+Description=etcd key-value store
+Documentation=https://github.com/coreos/etcd
+After=network.target
+
+[Service]
+Environment=ETCD_DATA_DIR=/var/lib/etcd
+Environment=ETCD_NAME=%m
+ExecStart=/usr/bin/etcd
+Restart=always
+RestartSec=10s
+LimitNOFILE=40000
+
+[Install]
+WantedBy=multi-user.target
+    | SERVICE
+
+package { 'etcd':
+  ensure => latest,
 } ->
-file { '/etc/nginx/html/index.html':
-	content => 'Hello from Puppet',
+file { '/etc/systemd/system/etcd.service':
+  ensure => present,
+  content => $etcd_service,
 } ~>
-service { 'nginx':
-	ensure => running,
-	enable => true,
+service { 'etcd':
+  ensure => running,
+  enable => true,
 }
